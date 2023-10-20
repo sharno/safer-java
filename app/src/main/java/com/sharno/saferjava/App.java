733b1be2f594d1463a3ea63515ae38eaa3b8399a
Mohamed Elsharnouby <sharnoby3@gmail.com>
2023-10-20 03:27:25 +0000

    destructuring

--- app/src/main/java/com/sharno/saferjava/App.java
+++ app/src/main/java/com/sharno/saferjava/App.java
@@ -5,14 +5,14 @@ import javax.annotation.Nonnull;
 sealed interface Status permits Pending, Shipped, Delivered {}
 record Pending() implements Status {}
 record Shipped(@Nonnull String carrier) implements Status {}
-record Delivered(@Nonnull Long deliveryTime) implements Status {}
+record Delivered(long deliveryTime) implements Status {}
 
 public class App {
     public static void main(String[] args) {
         var message = switch (getOrderStatus()) {
             case Pending p -> "The order is pending";
-            case Shipped s -> String.format("Order shipped with carrier: %s", s.carrier());
-            case Delivered d -> String.format("Order delivered at timestamp: %d", d.deliveryTime());
+            case Shipped(String carrier) -> String.format("Order shipped with carrier: %s", carrier);
+            case Delivered(long deliveryTime) -> String.format("Order delivered at timestamp: %d", deliveryTime);
         };
         System.out.println(message);
     }
